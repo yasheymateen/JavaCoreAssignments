36dd005e2888fb0a5d18c43cd16017d6830d5ae3
yasheymateen <yasheymateen@gmail.com>
2020-12-08 14:27:32 -0500

    added Pattern file and GuessingGame file

--- Pattern.java
+++ Pattern.java
@@ -17,43 +17,40 @@ public class Pattern {
 		leftDescending();
 	}
 		
+	public static void leftPattern() {
+	// initialize i and j for lines and columns
+		int i, j, lines=4;
 		
-		
-		public static void leftPattern() {
-		// initialize i and j for lines and columns
-			int i, j, lines=4;
-		
-		// outer loop for number of lines
-			for(i=0; i<lines; i++) {
-			// inner loop for # of stars in each line
-				for(j=0; j<=i; j++) {
-					System.out.print("*");
-				}
-				System.out.println();
+	// outer loop for number of lines
+		for(i=0; i<lines; i++) {
+		// inner loop for # of stars in each line
+			for(j=0; j<=i; j++) {
+				System.out.print("*");
 			}
+			System.out.println();
 		}
+	}
 		
-		public static void pyramidPattern() {
-			int i, j, lines = 4;
-			for (i = 0; i<lines; i++) {
-				for(j=lines-i; j>1; j--) {
-					System.out.print(" ");
-				}
-				for(j=0; j<=i; j++) {
-					System.out.print("* ");
-				}
-				System.out.println();
+	public static void pyramidPattern() {
+		int i, j, lines = 4;
+		for (i = 0; i<lines; i++) {
+			for(j=lines-i; j>1; j--) {
+				System.out.print(" ");
 			}
+			for(j=0; j<=i; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
 		}
+	}
 		
-		public static void leftDescending() {
-			int i, j, lines = 4;
-			for(i=lines-1; i>=0; i--) {
-				for(j=0; j<=i; j++) {
-					System.out.print("*" + " ");
-				}
-				System.out.println();
+	public static void leftDescending() {
+		int i, j, lines = 4;
+		for(i=lines-1; i>=0; i--) {
+			for(j=0; j<=i; j++) {
+				System.out.print("*");
 			}
+			System.out.println();
 		}
-
+	}
 }
